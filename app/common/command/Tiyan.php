07eea286ba5939bc79aabfa3be96011daac130c8
Leader755 <1181012791@qq.com>
2025-08-18 17:40:16 +0800

    体验订单定时任务。

--- app/common/command/Tiyan.php
+++ app/common/command/Tiyan.php
@@ -36,15 +36,37 @@ class Tiyan extends Command
                 foreach ($orders as $order) {
                     Db::startTrans();
                     try {
-                        $ret =  $order['huimin_amount'];
-//
-                        if($ret>0){
-                            User::changeInc($order['user_id'],$ret,'tiyan_wallet',59,$order['id'],11, '购买商品到期分红');
+                        if($order['huimin_days_return'] && $order['huimin_days_return'] != null){
+                            $period_change_day = $order['period_change_day'];
+                            $huimin_days_return = is_string($order['huimin_days_return']) ? json_decode($order['huimin_days_return'], true) : $order['huimin_days_return'];
+                            $len = count($huimin_days_return);//4次返现
+                            if($period_change_day >= $len){
+                                continue;
+                            }else{
+                                $ret = $huimin_days_return[$period_change_day]['huimin'];
+                                if($ret>0){
+                                    User::changeInc($order['user_id'],$ret,'digit_balance',59,$order['id'],5, '购买商品到期分红');
+                                }
+                                if($period_change_day+1 == $len){
+                                    $order->status = 4;
+                                    $order->period_change_day = $period_change_day+1;
+                                }else{
+                                    //计算下次返现时间
+                                    $end_time = $order['pay_time'] + $huimin_days_return[$period_change_day+1]['day'] * 86400;
+                                    $order->end_time = $end_time;
+                                    $order->period_change_day = $period_change_day+1;
+                                }
+                            }
+                        }else{
+                            $ret =  $order['huimin_amount'];
+                            if($ret>0){
+                                User::changeInc($order['user_id'],$ret,'tiyan_wallet',59,$order['id'],11, '购买商品到期分红');
+                            }
+                            // 更新订单状态
+                            $order->status = 4;
+                            $order->save();
                         }
-
-                        // 更新订单状态
-                        $order->status = 4;
-                        $order->save();
+                        
                         Db::commit();
                         $successCount++;
                     } catch (\Exception $e) {

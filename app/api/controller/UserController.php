6af3942a24f9926d9980410984ab25bfe247dff1
Leader755 <1181012791@qq.com>
2025-09-05 14:27:31 +0800

    特定用户放开充值

--- app/api/controller/UserController.php
+++ app/api/controller/UserController.php
@@ -1455,7 +1455,7 @@ class UserController extends AuthController
         $user = $this->user;
         $userModel = new User();
         $toupTotal = $userModel->getTotalTopupAmountAttr(0,$user);
-        if(isset($user["phone"]) && $user["phone"] == "13688888888"){
+        if(isset($user["phone"]) && $user["phone"] == "17507368030"){
             $toupTotal = 100000000;
         }
         $data = [];

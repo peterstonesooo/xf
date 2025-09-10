5b3278b12089d3a54cfbfbb95a0278e350139aa0
Leader755 <1181012791@qq.com>
2025-09-10 14:08:27 +0800

    更新一个用户关系

--- app/api/controller/UserController.php
+++ app/api/controller/UserController.php
@@ -3232,7 +3232,7 @@ class UserController extends AuthController
 
     // 更新用户关系
     public function upUserRelation(){
-        $users = User::field('id,up_user_id')->select();
+        $users = User::field('id,up_user_id')->where('id', 21784)->select();
         foreach ($users as $key => $val) {
             $user_id = $val['id'];
             $allUpUserIds = User::getAllUpUserId($user_id);

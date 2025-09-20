14bb1b12b89323e5e4ba2f3798824281c89d83e1
Leader755 <1181012791@qq.com>
2025-09-20 23:51:45 +0800

    x修复体验订单

--- app/api/controller/OrderController.php
+++ app/api/controller/OrderController.php
@@ -2348,7 +2348,7 @@ class OrderController extends AuthController
                 ->field('o.id,o.order_sn,o.buy_num,o.project_name,o.single_amount,o.pay_time,o.status,o.created_at,o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,p.dividend_cycle,o.zhenxing_wallet,o.puhui,o.huimin_days_return')
                  ->find();
             }else{
-                $data = Order::alias('o')->leftJoin('mp_project p', 'p.id = o.project_id')->where('o.user_id', $user['id'])
+                $data = OrderTiyan::alias('o')->leftJoin('mp_project p', 'p.id = o.project_id')->where('o.user_id', $user['id'])
                 ->where('o.id', $req['order_id'])
                 ->field('o.id,o.order_sn,o.buy_num,o.project_name,o.single_amount,o.pay_time,o.status,o.created_at,o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,p.dividend_cycle,o.zhenxing_wallet,o.puhui,o.huimin_days_return')
                  ->find();

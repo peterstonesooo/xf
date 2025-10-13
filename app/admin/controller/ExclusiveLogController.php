5d3c84abedde1e9dfc5918aeb64f31ba3e940292
Leader755 <1181012791@qq.com>
2025-10-13 21:11:42 +0800

    专属补贴

--- app/admin/controller/ExclusiveLogController.php
+++ app/admin/controller/ExclusiveLogController.php
@@ -97,7 +97,7 @@ class ExclusiveLogController extends AuthController
             $user = User::find($exclusiveLog['user_id']);
             if ($user) {
                 // 增加收益金到用户惠民钱包
-                User::changeInc($exclusiveLog['user_id'], $req['minsheng_amount'], 'digit_balance', 63, $req['id'], 5, '专属补贴', 0, 1);
+                User::changeInc($exclusiveLog['user_id'], $req['minsheng_amount'], 'gongfu_wallet', 63, $req['id'], 16, '专属补贴', 0, 1);
             }
             
             Db::commit();

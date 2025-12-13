6970997e31a12ff58335837a910cf9966f6bdc8e
Leader755 <1181012791@qq.com>
2025-12-13 18:19:01 +0800

    no message

--- app/api/controller/OrderController.php
+++ app/api/controller/OrderController.php
@@ -2043,20 +2043,23 @@ class OrderController extends AuthController
                 $data = OrderDailyBonus::alias('o')->leftJoin('mp_project p', 'p.id = o.project_id')->where('o.user_id', $user['id'])
                 ->where('o.id', $req['order_id'])
                 ->field('o.id,o.order_sn,o.buy_num,o.project_name,o.single_amount,o.pay_time,o.status,o.created_at,
-                o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,
+                o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,o.project_id,
                 p.dividend_cycle,o.zhenxing_wallet,o.puhui,o.huimin_days_return,o.yuding_amount,o.return_type,o.gongfu_right_now,o.zhenxing_right_now,p.minsheng_right_now,p.gold_right_now')
                  ->find();
             }else{
                 if($req['project_id'] == 3){
                     $data = OrderTiyan::alias('o')->leftJoin('mp_project p', 'p.id = o.project_id')->where('o.user_id', $user['id'])
                     ->where('o.id', $req['order_id'])
-                    ->field('o.id,o.order_sn,o.buy_num,o.project_name,o.single_amount,o.pay_time,o.status,o.created_at,o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,p.dividend_cycle,o.zhenxing_wallet,o.puhui,o.huimin_days_return,p.minsheng_right_now,p.gold_right_now')
+                    ->field('o.id,o.order_sn,o.buy_num,o.project_name,o.single_amount,o.pay_time,
+                    o.status,o.created_at,o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,
+                    o.minsheng_amount,p.dividend_cycle,o.zhenxing_wallet,o.puhui,o.huimin_days_return,o.project_id,
+                    p.minsheng_right_now,p.gold_right_now')
                      ->find();
                 }else{
                     $data = Order::alias('o')->leftJoin('mp_project p', 'p.id = o.project_id')->where('o.user_id', $user['id'])
                     ->where('o.id', $req['order_id'])
                     ->field('o.id,o.order_sn,o.buy_num,o.project_name,o.single_amount,o.pay_time,o.status,o.created_at,
-                    o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,p.dividend_cycle,
+                    o.pay_method,o.period,o.huimin_amount shoyi,o.gongfu_amount buzhu,o.minsheng_amount,p.dividend_cycle,o.project_id,
                     o.zhenxing_wallet,o.puhui,o.huimin_days_return,o.yuding_amount,o.return_type,o.gongfu_right_now,o.zhenxing_right_now,p.minsheng_right_now,p.gold_right_now')
                      ->find();
                 }
